29f6b4b2d83a39ae9502942c40eaff4e28363b89
Jonathan Ventura <jonathan.venturarj@gmail.com>
2022-10-01 15:37:53 -0300

    Update Logica - Desafio 2 (Facil).java

--- Logica - Desafio 2 (Facil).java
+++ Logica - Desafio 2 (Facil).java
@@ -1,16 +1,24 @@
-public class Desafio2 {
-    public static void main(String[] args) {
-        int a, n ;
+// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.  
 
-        a = 5;
-        n = 20;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int A, N;
+
+        Scanner input = new Scanner(System.in);
+        A = input.nextInt();
+        N = input.nextInt();
+
+        //TODO:  Retorne o valor da soma de todos múltiplos de "A" até o seu limite "N".
         int soma = 0;
-        for (int i = 0; i <= n; i++) {
-            if (i % a == 0){
+        for (int i = 0; i <= N; i++) {
+            if (i % A == 0){
                 soma += i;
             }
         }
         System.out.println(soma);
-
     }
 }
